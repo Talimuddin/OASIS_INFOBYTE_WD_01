cd7012406eacbe18d04f021c0daaa921f5aea05d
Talimuddin <talimuddin2000@gmail.com>
2024-07-24 21:08:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function learnMore() {
+    document.getElementById('about').scrollIntoView({ behavior: 'smooth' });
+}
+
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for contacting us!');
+    this.reset();
+});
